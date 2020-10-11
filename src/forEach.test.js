008757da59c25a2dd5cc8386e3a82fac6f7e8702
Mateusz Choma <kontakt@coderoad.pl>
2020-10-11 19:26:23 +0200

    Testing callaback in forEach with closure based mock

--- src/forEach.test.js
+++ src/forEach.test.js
@@ -1,16 +1,25 @@
+const makeTest = () => {
+    let numberOfCalls = 0
+
+    const test = (el, i, arr) => {
+        numberOfCalls = numberOfCalls + 1
+    }
+    const get = () => numberOfCalls
+
+    return { test, get }
+}
+
 describe('Array.prototype.forEach', () => {
 
     it('should call callback function X times for X length array v1', () => {
 
         const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-        let numberOfCalls = 0
+        const { test, get } = makeTest()
 
-        numbers.forEach((el, i, arr) => {
-            numberOfCalls = numberOfCalls + 1
-        })
+        numbers.forEach(test)
 
-        expect(numberOfCalls).toBe(numbers.length)
+        expect(get()).toBe(numbers.length)
 
     })
 
@@ -18,13 +27,11 @@ describe('Array.prototype.forEach', () => {
 
         const names = ['Ala', 'Ola', 'Ela']
 
-        let numberOfCalls = 0
+        const { test, get } = makeTest()
 
-        names.forEach((el, i, arr) => {
-            numberOfCalls = numberOfCalls + 1
-        })
+        names.forEach(test)
 
-        expect(numberOfCalls).toBe(names.length)
+        expect(get()).toBe(names.length)
 
     })
 
